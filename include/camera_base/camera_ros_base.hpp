4e9708a970d173fcb1eab57d851e5cabebbcdcab
Felipe Gomes de Melo <felipegmelo.42@gmail.com>
2022-02-02 05:24:44 -0300

    [camera_ros_base] Change ROS1 calls to ROS2

--- include/camera_base/camera_ros_base.hpp
+++ include/camera_base/camera_ros_base.hpp
@@ -10,46 +10,30 @@
 #include <sensor_msgs/msg/image.hpp>
 
 namespace camera_base {
-
-/**
- * @brief getParam Util function for getting ros parameters under nodehandle
- * @param nh Node handle
- * @param name Parameter name
- * @return Parameter value
- */
-template <typename T>
-T getParam(const ros::NodeHandle& nh, const std::string& name) {
-  T value{};
-  if (!nh.getParam(name, value)) {
-    ROS_ERROR("Cannot find parameter: %s", name.c_str());
-  }
-  return value;
-}
-
 /**
  * @brief The CameraRosBase class
  * This class implements a ros camera
  */
 class CameraRosBase {
  public:
-  explicit CameraRosBase(const ros::NodeHandle& pnh,
+  explicit CameraRosBase(rclcpp::Node::SharedPtr node,
+                         const std::string& frame_id_ = std::string(),
+                         const std::string& identifier_ = std::string(),
+                         const std::string& camera_name = std::string(),
+                         const std::string& calib_url = std::string(),
                          const std::string& prefix = std::string())
-      : pnh_(pnh),
-        cnh_(pnh, prefix),
-        it_(cnh_),
+      : it_(node),
         camera_pub_(it_.advertiseCamera("image_raw", 1)),
-        cinfo_mgr_(cnh_, getParam<std::string>(cnh_, "camera_name"),
-                   getParam<std::string>(cnh_, "calib_url")),
+        cinfo_mgr_(node.get(), camera_name, calib_url),
         fps_(10.0),
-        diagnostic_updater_(pnh_, cnh_),
+        frame_id_(frame_id_),
+        identifier_(identifier_),
+        diagnostic_updater_(node),
         topic_diagnostic_(
             prefix.empty() ? "image_raw" : (prefix + "/image_raw"),
             diagnostic_updater_,
             diagnostic_updater::FrequencyStatusParam(&fps_, &fps_, 0.1, 10),
-            diagnostic_updater::TimeStampStatusParam(-0.01, 0.1)) {
-    cnh_.param<std::string>("frame_id", frame_id_, cnh_.getNamespace());
-    cnh_.param<std::string>("identifier", identifier_, "");
-  }
+            diagnostic_updater::TimeStampStatusParam(-0.01, 0.1)){};
 
   CameraRosBase() = delete;
   CameraRosBase(const CameraRosBase&) = delete;
@@ -74,10 +58,10 @@ class CameraRosBase {
    * @brief PublishCamera Publish a camera topic with Image and CameraInfo
    * @param time Acquisition time stamp
    */
-  void PublishCamera(const ros::Time& time) {
-    const auto image_msg = std::make_shared<sensor_msgs::Image>();
-    const auto cinfo_msg =
-        std::make_shared<sensor_msgs::CameraInfo>(cinfo_mgr_.getCameraInfo());
+  void PublishCamera(const rclcpp::Time& time) {
+    const auto image_msg = std::make_shared<sensor_msgs::msg::Image>();
+    const auto cinfo_msg = std::make_shared<sensor_msgs::msg::CameraInfo>(
+        cinfo_mgr_.getCameraInfo());
     image_msg->header.frame_id = frame_id_;
     image_msg->header.stamp = time;
     if (Grab(image_msg, cinfo_msg)) {
@@ -86,18 +70,22 @@ class CameraRosBase {
       camera_pub_.publish(image_msg, cinfo_msg);
       topic_diagnostic_.tick(image_msg->header.stamp);
     }
-    diagnostic_updater_.update();
+
+    // TODO! Should this be done?
+    diagnostic_updater_.force_update();
   }
 
-  void Publish(const sensor_msgs::ImagePtr& image_msg) {
-    const auto cinfo_msg =
-        std::make_shared<sensor_msgs::CameraInfo>(cinfo_mgr_.getCameraInfo());
+  void Publish(const sensor_msgs::msg::Image::SharedPtr& image_msg) {
+    const auto cinfo_msg = std::make_shared<sensor_msgs::msg::CameraInfo>(
+        cinfo_mgr_.getCameraInfo());
     // Update camera info header
     image_msg->header.frame_id = frame_id_;
     cinfo_msg->header = image_msg->header;
     camera_pub_.publish(image_msg, cinfo_msg);
     topic_diagnostic_.tick(image_msg->header.stamp);
-    diagnostic_updater_.update();
+
+    // TODO! Should this be done?
+    diagnostic_updater_.force_update();
   }
 
   /**
@@ -105,20 +93,19 @@ class CameraRosBase {
    * @param image_msg Ros message ImagePtr
    * @return True if successful
    */
-  virtual bool Grab(const sensor_msgs::ImagePtr& image_msg,
-                    const sensor_msgs::CameraInfoPtr& cinfo_msgs = nullptr) = 0;
+  virtual bool Grab(
+      const sensor_msgs::msg::Image::SharedPtr& image_msg,
+      const sensor_msgs::msg::CameraInfo::SharedPtr& cinfo_msgs = nullptr) = 0;
 
  private:
-  ros::NodeHandle pnh_;
-  ros::NodeHandle cnh_;
   image_transport::ImageTransport it_;
   image_transport::CameraPublisher camera_pub_;
   camera_info_manager::CameraInfoManager cinfo_mgr_;
   double fps_;
-  diagnostic_updater::Updater diagnostic_updater_;
-  diagnostic_updater::TopicDiagnostic topic_diagnostic_;
   std::string frame_id_;
   std::string identifier_;
+  diagnostic_updater::Updater diagnostic_updater_;
+  diagnostic_updater::TopicDiagnostic topic_diagnostic_;
 };
 
 }  // namespace camera_base
